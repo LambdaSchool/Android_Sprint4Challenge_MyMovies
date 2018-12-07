adc0a497297fc51a29955cff8e34e97dc7eddbc8
Patrick Martin <patrickmartin88@gmail.com>
2018-12-07 13:40:39 -0800

    Implementing FavoriteMoviesActivity.

--- app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/FavoriteMoviesActivity.java
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/FavoriteMoviesActivity.java
@@ -1,13 +1,50 @@
 package com.lambdaschool.sprint4challenge_mymovies;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class FavoriteMoviesActivity extends AppCompatActivity {
+
+    private Context context;
+    private LinearLayout favesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_movies);
+
+
+        favesList = findViewById(R.id.fave_linear_layout);
+
+
+        ArrayList<FavoriteMovie> chosenMovies = new ArrayList<>();
+        chosenMovies = MovieDbDao.readFaveMovies();
+        for (FavoriteMovie temp : chosenMovies) {
+            final FavoriteMovie movie = temp;
+            final TextView tv = new TextView(this);
+            tv.setText(movie.toString());
+            tv.setTextSize(22);
+            tv.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    MovieDbDao.deleteMovie(movie.getTitle());
+                    tv.setVisibility(View.GONE);
+                    return false;
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
     }
 }
