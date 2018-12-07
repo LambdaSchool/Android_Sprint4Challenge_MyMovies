f94addf8c56ee84a5c0038388498dc54cb39683e
ThaDocIzN <rodan72@msn.com>
2018-12-07 12:37:34 -0600

    changed name movieDbDao to movieApiDao

--- app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/activities/MainActivity.java
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/activities/MainActivity.java
@@ -1,29 +1,57 @@
 package com.lambdaschool.sprint4challenge_mymovies.activities;
 
-import android.graphics.Bitmap;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
 
 import com.lambdaschool.sprint4challenge_mymovies.R;
-import com.lambdaschool.sprint4challenge_mymovies.apiaccess.MovieDbDao;
+import com.lambdaschool.sprint4challenge_mymovies.RecycleAdapter;
+import com.lambdaschool.sprint4challenge_mymovies.apiaccess.MovieApiDao;
 import com.lambdaschool.sprint4challenge_mymovies.apiaccess.MovieOverview;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<MovieOverview> overviews;
+    RecyclerView recyclerView;
+    Context context;
+    RecycleAdapter adapter;
+    EditText keyWordSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Thread(new Runnable() {
+        context = this;
+        overviews = new ArrayList<>();
+        keyWordSearch = findViewById(R.id.etSearchKeyword);
+        recyclerView =findViewById(R.id.recyclerViewer);
+
+        findViewById(R.id.imageView).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                overviews = MovieDbDao.searchMovies("batman");
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        overviews = MovieApiDao.searchMovies(keyWordSearch.getText().toString());
+                    }
+                }).start();
             }
-        }).start();
+        });
+
+        adapter = new RecycleAdapter(overviews);
+
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(manager);
+        DividerItemDecoration decoration = new DividerItemDecoration(context, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(decoration);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
     }
 }
