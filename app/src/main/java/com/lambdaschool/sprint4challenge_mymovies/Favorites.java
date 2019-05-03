1bd5b27044029b17f92689e69b3e600c730eebde
BL-CoderOvertime <brandonlively@live.com>
2019-05-03 11:15:56 -0400

    Setup activity_favorites layout

---
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/Favorites.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.sprint4challenge_mymovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Favorites extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_favorites);
+	}
+}
