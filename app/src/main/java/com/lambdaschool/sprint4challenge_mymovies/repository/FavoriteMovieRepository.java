774baf67c5f55da08d0d7231d36a198b35592a4f
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-03 10:34:36 -0500

    FavoriteMovieRepository and FavoriteMoviesViewModel defined

--- app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/repository/FavoriteMovieRepository.java
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/repository/FavoriteMovieRepository.java
@@ -1,4 +1,27 @@
 package com.lambdaschool.sprint4challenge_mymovies.repository;
 
+import android.arch.lifecycle.MutableLiveData;
+
+import com.lambdaschool.sprint4challenge_mymovies.dao.FavoriteMovieDao;
+import com.lambdaschool.sprint4challenge_mymovies.model.FavoriteMovie;
+
+import java.util.ArrayList;
+
 public class FavoriteMovieRepository {
+
+    private FavoriteMovieDao favoriteMovieDao;
+    private ArrayList<FavoriteMovie> favoriteMovies;
+
+    public FavoriteMovieRepository() {
+        favoriteMovieDao = new FavoriteMovieDao();
+    }
+
+    public MutableLiveData<ArrayList<FavoriteMovie>> getData() {
+        favoriteMovies = favoriteMovieDao.getFavoriteMovies();
+        MutableLiveData<ArrayList<FavoriteMovie>> liveData = new MutableLiveData<>();
+
+        liveData.setValue(favoriteMovies);
+        return liveData;
+    }
+
 }
--- app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/view_model/FavoriteMoviesViewModel.java
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/view_model/FavoriteMoviesViewModel.java
@@ -1,4 +1,30 @@
 package com.lambdaschool.sprint4challenge_mymovies.view_model;
 
-public class FavoriteMoviesViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.lambdaschool.sprint4challenge_mymovies.model.FavoriteMovie;
+import com.lambdaschool.sprint4challenge_mymovies.repository.FavoriteMovieRepository;
+
+import java.util.ArrayList;
+
+public class FavoriteMoviesViewModel extends ViewModel {
+
+    private MutableLiveData<ArrayList<FavoriteMovie>> liveData;
+    private FavoriteMovieRepository repository;
+
+    public LiveData<ArrayList<FavoriteMovie>> getData() {
+        if (liveData == null) {
+            loadData();
+        }
+
+        return liveData;
+    }
+
+    private void loadData() {
+        repository = new FavoriteMovieRepository();
+        liveData = repository.getData();
+    }
+
 }
