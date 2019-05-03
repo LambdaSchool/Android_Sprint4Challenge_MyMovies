4bcc3ab85fa40bb76eb3ae26f037f08948a25ac5
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-03 10:11:29 -0500

    defined FavoriteMovie

--- app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/model/FavoriteMovie.java
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/model/FavoriteMovie.java
@@ -1,4 +1,36 @@
 package com.lambdaschool.sprint4challenge_mymovies.model;
 
 public class FavoriteMovie {
+
+    private long id;
+    private String title;
+    private String imageUrl;
+    private boolean isWatched;
+
+    public FavoriteMovie(long id, String title, String imageUrl, boolean isWatched) {
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.isWatched = isWatched;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public boolean isWatched() {
+        return isWatched;
+    }
+
+    public void setWatched(boolean watched) {
+        isWatched = watched;
+    }
 }
