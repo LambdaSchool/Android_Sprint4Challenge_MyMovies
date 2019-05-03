61472d307731c20f65f3c0c89902b60465bbfffd
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-03 10:29:40 -0500

    FavoriteMovieDao defined

--- app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/dao/FavoriteMovieDao.java
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/dao/FavoriteMovieDao.java
@@ -1,4 +1,21 @@
 package com.lambdaschool.sprint4challenge_mymovies.dao;
 
+import com.lambdaschool.sprint4challenge_mymovies.model.FavoriteMovie;
+import com.lambdaschool.sprint4challenge_mymovies.sqldb.FavoriteMovieDbHelper;
+
+import java.util.ArrayList;
+
 public class FavoriteMovieDao {
+
+    public void addFavoriteMovie(FavoriteMovie favoriteMovie) {
+        FavoriteMovieDbHelper.createFavoriteMovie(favoriteMovie);
+    }
+
+    public void removeFavoriteMovie(FavoriteMovie favoriteMovie) {
+        FavoriteMovieDbHelper.deleteFavoriteMovie(favoriteMovie.getId());
+    }
+
+    public ArrayList<FavoriteMovie> getFavoriteMovies() {
+        return FavoriteMovieDbHelper.readFavoriteMovies();
+    }
 }
