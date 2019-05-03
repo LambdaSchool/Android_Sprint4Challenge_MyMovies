f5a8bfd95ebb469ad1f688c73e66ab9e8fd9725e
VivekV95 <vivekv210@gmail.com>
2019-05-03 10:17:22 -0500

    Added db contract class

--- app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/FavoriteMovieDbContract.java
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/FavoriteMovieDbContract.java
@@ -1,4 +1,24 @@
 package com.lambdaschool.sprint4challenge_mymovies;
 
-public class FavoriteMovieDbContract {
+import android.provider.BaseColumns;
+
+    public class FavoriteMovieDbContract {
+        public static class FavoriteMovieEntry implements BaseColumns {
+            public static final String TABLE_NAME = "favorite_movies";
+            public static final String COLUMN_NAME_TITLE = "title";
+            public static final String COLUMN_NAME_OVERVIEW = "overview";
+            public static final String COLUMN_NAME_RELEASE_DATE = "release_date";
+            public static final String COLUMN_NAME_WATCHED = "watched";
+
+            public static final String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME
+                    +" ( "
+                    + _ID + " INTEGER PRIMARY KEY, "
+                    + COLUMN_NAME_TITLE + " TEXT, "
+                    + COLUMN_NAME_OVERVIEW + " TEXT, "
+                    + COLUMN_NAME_RELEASE_DATE + " TEXT, "
+                    + COLUMN_NAME_WATCHED + " INTEGER);";
+
+            public static final String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS "
+                    + TABLE_NAME + ";";
+        }
 }
