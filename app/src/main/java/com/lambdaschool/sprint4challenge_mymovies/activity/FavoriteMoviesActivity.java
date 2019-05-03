d1adb8f07b76aaf76cb69d26db69412365a14313
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-03 10:36:38 -0500

    added FavoriteMoviesActivity

--- app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/activity/FavoriteMoviesActivity.java
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/activity/FavoriteMoviesActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 
 import com.lambdaschool.sprint4challenge_mymovies.R;
 
-public class MainActivity extends AppCompatActivity {
+public class FavoriteMoviesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_favorites);
 
 
     }
