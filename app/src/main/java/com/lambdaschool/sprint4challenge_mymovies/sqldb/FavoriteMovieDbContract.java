b1a76c91f2cb149663c3dec73c78da3b720eba4b
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-03 10:16:33 -0500

    FavoriteMovieDbContract defined

--- app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/sqldb/FavoriteMovieDbContract.java
+++ app/src/main/java/com/lambdaschool/sprint4challenge_mymovies/sqldb/FavoriteMovieDbContract.java
@@ -1,4 +1,21 @@
 package com.lambdaschool.sprint4challenge_mymovies.sqldb;
 
+import android.provider.BaseColumns;
+
 public class FavoriteMovieDbContract {
+
+    public static final String SQL_CREATE_TABLE =
+            "CREATE TABLE " + FavoriteMovieEntry.TABLE_NAME + " (\n" +
+                    FavoriteMovieEntry._ID + " INTEGER PRIMARY KEY,\n" +
+                    FavoriteMovieEntry.COLUMN_NAME_TITLE + " TEXT,\n" +
+                    FavoriteMovieEntry.COLUMN_NAME_IMAGE_URL + " TEXT,\n" +
+                    FavoriteMovieEntry.COLUMN_NAME_IS_WATCHED + " INTEGER);";
+    public static final String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS " + FavoriteMovieEntry.TABLE_NAME + ";";
+
+    public class FavoriteMovieEntry implements BaseColumns {
+        public static final String TABLE_NAME = "FavoriteMovies";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_IMAGE_URL = "image_url";
+        public static final String COLUMN_NAME_IS_WATCHED = "is_watched";
+    }
 }
