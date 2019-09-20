c785ea8e7cf977f998867f869f1362caebeea0f4
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-20 17:43:15 -0600

    Movie(listResp)
    this is just a respretory to hold the movies in

---
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/ListRespritory.kt
@@ -0,0 +1,17 @@
+package com.lambdaschool.datapersistencesprintchallenge
+
+import com.lambdaschool.sprint4challenge_mymovies.apiaccess.MovieConstants.MOVIE_NAMES
+import com.lambdaschool.sprint4challenge_mymovies.model.MovieOverview
+
+class ListRespritory{
+    companion object{
+
+        var createList = mutableListOf<MovieOverview>()
+
+
+        fun createView(){
+            for (index in 0 until MOVIE_NAMES.size)
+                createList.add(MovieOverview(MOVIE_NAMES[index].toInt(), MOVIE_NAMES[index]))
+        }
+    }
+}
--- app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/apiaccess/MovieConstants.kt
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/apiaccess/MovieConstants.kt
@@ -5,4 +5,5 @@ object MovieConstants {
     const val API_KEY_PARAM = "359211348348a13a2b996217f7538f45"
     const val FIXED_QUERY_PARAMS = "?language=en-US&page=1&include_adult=false"
     const val SEARCH_MOVIES_ENDPOINT = "search/movie$FIXED_QUERY_PARAMS"
+    var MOVIE_NAMES = arrayOf("test", "test")
 }
